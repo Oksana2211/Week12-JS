69d4e68179fb05241d65db6da43915751d543471
Тайсумова Оксана <119331948+Oksana2211@users.noreply.github.com>
2023-06-15 10:35:36 +0300

    fetch.then.catch

--- index.js
+++ index.js
@@ -10,9 +10,32 @@ function Conclusion(e) {
   let activOption = option.value; // записываем значение из select-->option в переменную
   let activNamber = namber.value; // записываем значение из input number в переменную
 
-  console.log(activOption);
-  console.log(activNamber);
 
+  fetch(`https://swapi.nomoreparties.co/${activOption}/${activNamber}`)
+    .then((response) => response.json())
+    .then((response) => {
+
+      document.querySelector(".result").innerHTML = `${response.name}`; // помещаем в div result имя объекта
+
+      if (document.querySelector(".result").textContent == 'undefined') {
+        document.querySelector(".result").innerHTML = "";
+      }
+
+      if (activNamber < 1 || activNamber > 10) {
+        throw new Error( // создаем ошибку 
+          "Ошибка. Введите число от 1 до 10 включительно."
+        );
+      };
+
+    }).catch((error) => {
+      document.querySelector(".result").innerHTML = `<p class="error">${error.message}</p>`; //выводим сообщение об ошибке
+
+    }).finally(() => {
+      console.log(activOption);
+      console.log(activNamber);
+      option.value = "";
+      namber.value = "";
+    });
 
 }
 
